737ad1880a428cf0f5ebeb9ba7ba6866643347c4
Veronica De Felice <verodefelic@gmail.com>
2023-04-04 12:27:42 -0400

    FIRST Firestore DB

---
+++ src/animals.js
@@ -0,0 +1,13 @@
+import { db } from './dbConnect.js'
+
+
+
+
+export function addNewAnimal(req, res) {
+const newAnimal = req.body
+db.collection('animals').add(newAnimal)
+.then(doc => res.status(201).send("New animal added: " + doc.id))
+.catch(err => res.status(500).send(err))
+
+}
+
